5eecbf45be620b1aa90e311bf0fe6e4350fcc082
hamdanafdhal <hamdaansaheer@gmail.com>
2026-02-01 23:16:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -43,8 +43,8 @@ const CONFIG = {
     // They show up depending on how far they slide the meter
     loveMessages: {
         extreme: "DANGG You love me that much?? 🥰🚀🐱​",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        high: "Wahoooooooooooooooooo! 🚀💝",              // Shows when they go past 1000%
+        normal: "Yippeeee! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
