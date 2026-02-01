0965b17ea7e1ab03bebb657e6a92bdfa99a37d28
hamdanafdhal <hamdaansaheer@gmail.com>
2026-02-01 23:19:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,10 +78,10 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dwustwgkk/video/upload/v1769968067/Bill_Withers_-_Just_The_Two_Of_Us_Lyrics_o5lx1o.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 0.4                        // Volume level (0.0 to 1.0)
     }
 };
 
